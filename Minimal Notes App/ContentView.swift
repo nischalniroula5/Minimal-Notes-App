7b9d1bf7484a006755cfa1f3e429fb900a1e444c
Nischal Niroula <officialnischalniroula@gmail.com>
2024-06-02 23:48:56 +1000

    Initial Commit

---
+++ Minimal Notes App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Minimal Notes App
+//
+//  Created by Nischal Niroula on 2/6/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Minimal Notes App/Minimal_Notes_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Minimal_Notes_AppApp.swift
+//  Minimal Notes App
+//
+//  Created by Nischal Niroula on 2/6/2024.
+//
+
+import SwiftUI
+
+@main
+struct Minimal_Notes_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
